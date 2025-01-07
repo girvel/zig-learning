3c4273d332580f88f7c470621ce85abee1db8b4a
girvel <widauka@ya.ru>
2025-01-07 22:20:19 +0500

    Implement displaying a game of life field

---
+++ src/main.zig
@@ -0,0 +1,22 @@
+const std = @import("std");
+const expect = std.testing.expect;
+
+pub fn main() void {
+    const game_field = [_]bool{
+        false, true,  false, true, false,
+        false, false, true,  true, true,
+        false, false, true,  true, true,
+        false, false, true,  true, true,
+        false, true,  false, true, false,
+    };
+    const game_field_width = 5;
+
+    for (game_field, 0..) |value, i| {
+        std.debug.print("{s}", .{if (value) "X" else "."});
+        if (i % game_field_width == (game_field_width - 1)) {
+            std.debug.print("\n", .{});
+        }
+    }
+}
+
+test "always succeeds" {}
