f290aa75dbbc1a03e2eec3f4ed6bc9a0c3a6042f
girvel <widauka@ya.ru>
2025-01-28 15:51:17 +0500

    Generics

--- src/main.zig
+++ src/main.zig
@@ -1,66 +1,66 @@
 const std = @import("std");
 const expect = std.testing.expect;
 
-const FIELD_SIZE = 25;
+fn mod_add(x: usize, offset: i8, limit: usize) usize {
+    return @intCast(@mod(@as(i8, @intCast(x)) + offset, @as(i8, @intCast(limit))));
+}
 
-const Gol = struct {
-    field: [FIELD_SIZE]i8,
-    width: usize,
+fn ProduceGol(comptime w: usize, comptime h: usize) type {
+    return struct {
+        const Self = @This();
 
-    fn display(self: Gol) void {
-        for (self.field, 0..) |value, i| {
-            std.debug.print("{s}", .{if (value == 1) "X" else "."});
-            if (i % self.width == (self.width - 1)) {
-                std.debug.print("\n", .{});
+        field: [w * h]i8,
+
+        fn display(self: Self) void {
+            for (self.field, 0..) |value, i| {
+                std.debug.print("{s}", .{if (value == 1) "X" else "."});
+                if (i % w == (w - 1)) {
+                    std.debug.print("\n", .{});
+                }
             }
         }
-    }
 
-    fn get_at(self: Gol, x: i8, y: i8) i8 {
-        const x_: usize = @intCast(@mod(x, @as(i8, @intCast(self.width))));
-        const y_: usize = @intCast(@mod(y, @as(i8, @intCast(self.field.len / self.width))));
-        return self.field[x_ + y_ * self.width];
-    }
-
-    fn set_at(self: *Gol, x: i8, y: i8, value: i8) void {  // TODO at
-        const x_: usize = @intCast(@mod(x, @as(i8, @intCast(self.width))));
-        const y_: usize = @intCast(@mod(y, @as(i8, @intCast(self.field.len / self.width))));
-        self.field[x_ + y_ * self.width] = value;
-    }
-
-    fn produce_next(self: Gol) Gol {
-        var result = Gol{ .field = undefined, .width = self.width };
-
-        for (0..self.width) |x_usize| {
-            for (0..self.field.len / self.width) |y_usize| {
-                const x: i8 = @intCast(x_usize);
-                const y: i8 = @intCast(y_usize);
-
-                const neighbours_n = (
-                    self.get_at(x - 1, y - 1) +
-                    self.get_at(x - 1, y) +
-                    self.get_at(x - 1, y + 1) +
-                    self.get_at(x, y - 1) +
-                    self.get_at(x, y + 1) +
-                    self.get_at(x + 1, y - 1) +
-                    self.get_at(x + 1, y) +
-                    self.get_at(x + 1, y + 1)
-                );
-                result.set_at(x, y,
-                    if (self.get_at(x, y) == 0)
-                        if (neighbours_n == 3) 1 else 0
-                    else
-                        if (neighbours_n < 2) 0
-                        else if (neighbours_n < 4) 1
-                        else 0
-                );
-            }
+        fn at(self: *Self, x: usize, y: usize) *i8 {
+            return &self.field[x + y * w];
         }
-        return result;
-    }
-};
+
+        fn get_at(self: Self, x: usize, y: usize) i8 {
+            return self.field[x + y * w];
+        }
+
+        fn produce_next(self: Self) Self {
+            var result = Self{ .field = undefined };
+
+            for (0..w) |x| {
+                for (0..h) |y| {
+                    const neighbours_n = (
+                        self.get_at(mod_add(x, -1, w), mod_add(y, -1, h)) +
+                        self.get_at(mod_add(x, -1, w), y) +
+                        self.get_at(mod_add(x, -1, w), mod_add(y, 1, h)) +
+                        self.get_at(x, mod_add(y, -1, h)) +
+                        self.get_at(x, mod_add(y, 1, h)) +
+                        self.get_at(mod_add(x, 1, w), mod_add(y, -1, h)) +
+                        self.get_at(mod_add(x, 1, w), y) +
+                        self.get_at(mod_add(x, 1, w), mod_add(y, 1, h))
+                    );
+
+                    result.at(x, y).* =
+                        if (self.get_at(x, y) == 0)
+                            if (neighbours_n == 3) 1 else 0
+                        else
+                            if (neighbours_n < 2) 0
+                            else if (neighbours_n < 4) 1
+                            else 0;
+                }
+            }
+
+            return result;
+        }
+    };
+}
 
 pub fn main() void {
+    const Gol = ProduceGol(5, 5);
     const gol = Gol{
         .field = [_]i8{
             0, 1, 0, 1, 0,
@@ -69,7 +69,6 @@ pub fn main() void {
             0, 0, 1, 1, 1,
             0, 1, 0, 1, 0,
         },
-        .width = 5,
     };
     gol.display();
 
