3f2278ff13234f8a026d861e205ddd691b65db96
girvel <widauka@ya.ru>
2025-01-28 12:29:57 +0500

    Functions

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,15 @@
 const std = @import("std");
 const expect = std.testing.expect;
 
+fn display_field(game_field: []const bool, width: usize) void {
+    for (game_field, 0..) |value, i| {
+        std.debug.print("{s}", .{if (value) "X" else "."});
+        if (i % width == (width - 1)) {
+            std.debug.print("\n", .{});
+        }
+    }
+}
+
 pub fn main() void {
     const game_field = [_]bool{
         false, true,  false, true, false,
@@ -10,11 +19,7 @@ pub fn main() void {
         false, true,  false, true, false,
     };
     const game_field_width = 5;
+    //const game_field_height = 5;
 
-    for (game_field, 0..) |value, i| {
-        std.debug.print("{s}", .{if (value) "X" else "."});
-        if (i % game_field_width == (game_field_width - 1)) {
-            std.debug.print("\n", .{});
-        }
-    }
+    display_field(&game_field, game_field_width);
 }
