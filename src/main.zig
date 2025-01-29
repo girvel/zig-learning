90d3dafa1dafea0c9dc5703194a36af1d7009eb5
girvel <widauka@ya.ru>
2025-01-29 19:20:21 +0500

    Reading from a file!!

--- src/main.zig
+++ src/main.zig
@@ -2,7 +2,24 @@ const std = @import("std");
 const rl = @import("raylib");
 const expect = std.testing.expect;
 
-pub fn main() void {
+pub fn main() anyerror!void {
+    var first_line: []u8 = undefined;
+    {
+        var file = try std.fs.cwd().openFile("demo.txt", .{});
+        defer file.close();
+
+        var buf_reader = std.io.bufferedReader(file.reader());
+        var in_stream = buf_reader.reader();
+
+        var buf: [1024]u8 = undefined;
+        while (try in_stream.readUntilDelimiterOrEof(&buf, '\n')) |line| {
+            first_line = line;
+            break;
+        }
+    }
+
+    std.debug.print("{s}\n", .{first_line});
+
     const w = 800;
     const h = 600;
 
