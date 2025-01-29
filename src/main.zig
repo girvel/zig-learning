818901f9eeb4bc790d486557c2448cb382f19e02
girvel <widauka@ya.ru>
2025-01-29 18:21:47 +0500

    Shorten the code

--- src/main.zig
+++ src/main.zig
@@ -1,35 +1,35 @@
 const std = @import("std");
-const raylib = @import("raylib");
+const rl = @import("raylib");
 const expect = std.testing.expect;
 
 pub fn main() void {
     const w = 800;
     const h = 600;
 
-    raylib.initWindow(w, h, "Game of Life");
-    defer raylib.closeWindow();
+    rl.initWindow(w, h, "Game of Life");
+    defer rl.closeWindow();
 
-    raylib.setTargetFPS(100);
+    rl.setTargetFPS(100);
 
-    const camera = raylib.Camera {
-        .position = raylib.Vector3{.x = 4, .y = 4, .z = 4},  // TODO test anonymous
-        .target = raylib.Vector3{.x = 0, .y = 0, .z = 0},
-        .up = raylib.Vector3{.x = 0, .y = 1, .z = 0},
+    const camera = rl.Camera {
+        .position = rl.Vector3.init(4, 4, 4),
+        .target = rl.Vector3.zero(),
+        .up = rl.Vector3.init(0, 1, 0),
         .fovy = 45,
-        .projection = raylib.CameraProjection.perspective,
+        .projection = rl.CameraProjection.perspective,
     };
 
-    while (!raylib.windowShouldClose()) {
-        raylib.beginDrawing();
-        defer raylib.endDrawing();
+    while (!rl.windowShouldClose()) {
+        rl.beginDrawing();
+        defer rl.endDrawing();
 
-        raylib.beginMode3D(camera);
-        defer raylib.endMode3D();
+        rl.beginMode3D(camera);
+        defer rl.endMode3D();
 
-        raylib.clearBackground(raylib.Color.white);
+        rl.clearBackground(rl.Color.white);
 
-        raylib.drawGrid(10, 1.0);
-        raylib.drawCube(raylib.Vector3 {.x = 0, .y = 0, .z = 0}, 1, 1, 1, raylib.Color.black);
-        raylib.drawFPS(10, 10);
+        rl.drawGrid(10, 1.0);
+        rl.drawCube(rl.Vector3.zero(), 1, 1, 1, rl.Color.black);
+        rl.drawFPS(10, 10);
     }
 }
