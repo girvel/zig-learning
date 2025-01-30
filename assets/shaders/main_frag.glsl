25f646051dbcc3e4b2b8df5e0cf04220c17029dc
girvel <widauka@ya.ru>
2025-01-30 15:33:47 +0500

    Make sides of cubes darker (closes #3)

--- assets/shaders/main_frag.glsl
+++ assets/shaders/main_frag.glsl
@@ -1,6 +1,7 @@
 #version 330
 
 in vec2 fragTexCoord;
+in vec3 fragNormal;
 in vec4 fragColor;
 
 uniform sampler2D texture0;
@@ -10,6 +11,10 @@ out vec4 finalColor;
 
 void main() {
     vec4 original = texture(texture0, fragTexCoord) * colDiffuse * fragColor;
-    float gray = dot(original.rgb, vec3(0.288, 0.587, 0.144));
-    finalColor = vec4(gray, gray, gray, original.a);
+    if (fragNormal.y > 0)
+        finalColor = original;
+    else if (fragNormal.y == 0)
+        finalColor = original * vec4(.8, .8, .8, 1);
+    else
+        finalColor = original * vec4(.6, .6, .6, 1);
 }
--- assets/shaders/main_vert.glsl
+++ assets/shaders/main_vert.glsl
@@ -1,6 +1,7 @@
 #version 330
 
 in vec3 vertexPosition;
+in vec2 vertexTexCoord;
 in vec3 vertexNormal;
 in vec4 vertexColor;
 
@@ -9,11 +10,13 @@ uniform mat4 matModel;
 uniform mat4 matNormal;
 
 out vec3 fragPosition;
+out vec2 fragTexCoord;
 out vec4 fragColor;
 out vec3 fragNormal;
 
 void main() {
     fragPosition = vec3(matModel * vec4(vertexPosition, 1.0));
+    fragTexCoord = vertexTexCoord;
     fragColor = vertexColor;
     fragNormal = normalize(vec3(matNormal * vec4(vertexNormal, 1.0)));
 
